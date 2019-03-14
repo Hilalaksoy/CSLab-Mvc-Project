4d21cf308bc15bedffc88c9627ec9c0ff2335bd3
Hilalaksoy <hll.aksy29@gmail.com>
2019-03-14 23:12:16 +0300

    Update

--- cslabWeb/cslabWeb/Areas/Admin/Controllers/AdminHomeController.cs
+++ cslabWeb/cslabWeb/Areas/Admin/Controllers/AdminHomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace cslabWeb.Areas.Admin.Controllers
@@ -11,6 +12,7 @@ namespace cslabWeb.Areas.Admin.Controllers
     
     public class AdminHomeController : Controller
     {
+        AdminContext db = new AdminContext();
         // GET: Admin/Home
         [UserAuthenticationFilter]
         public ActionResult Index()
@@ -28,11 +30,12 @@ namespace cslabWeb.Areas.Admin.Controllers
         }
          
         [HttpPost]
-        public ActionResult Login(AdminCs admin)
+        public ActionResult Login(AdminCs admin,string Password)
         {
             using (AdminContext db = new AdminContext())
             {
-                var adm = db.admins.SingleOrDefault(u => u.Username == admin.Username && u.Password == admin.Password);
+                var md5pass = Crypto.Hash(Password,"MD5");
+                var adm = db.admins.SingleOrDefault(u => u.Username == admin.Username && u.Password ==md5pass);
                 if (adm != null)
                 {
                     Session["Id"] = Guid.NewGuid();
@@ -53,6 +56,27 @@ namespace cslabWeb.Areas.Admin.Controllers
         {                    
            return RedirectToAction("Index");          
         }
+        public ActionResult Logout()
+        {
+            Session["Id"] = null;
+            Session.Abandon();
+            return RedirectToAction("Login");
+        }
 
+        //[UserAuthenticationFilter]
+        public ActionResult Create(AdminCs admin,string Password)
+        {
+            var md5pass = Password;
+            if (ModelState.IsValid)
+            {
+                admin.Password = Crypto.Hash(md5pass, "MD5");
+                db.admins.Add(admin);
+                db.SaveChanges();
+                Session["Id"] = admin.Id;
+                Session["Username"] = admin.Username;
+                return RedirectToAction("Login");
+            }            
+            return View();
+        }
     }
 }
